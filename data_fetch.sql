9be9ca45d7b07a0dcecf1b9437a1d9225177c7cd
DinLei <31896634+DinLei@users.noreply.github.com>
2019-01-15 10:17:00 +0800

    Update data_fetch.sql

--- data_fetch.sql
+++ data_fetch.sql
@@ -1,4 +1,4 @@
-create table if not exists ai_tmp.nlp_products_time_series_dl (
+create table if not exists your.table_name (
     product_id      bigint,
     category_id     int,
     ai_date         string,
@@ -19,6 +19,6 @@ sum(view_count) AS click_uv,
 sum(wish_count) AS wish_uv,
 sum(cart_count) AS cart_uv,
 sum(order_count) AS order_num
-FROM ai.nlp_category_action_static_daily_nc
+FROM your.table_name
 GROUP BY dctime, category_id, product_id
 ORDER BY product_id, category_id, dctime
